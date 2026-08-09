eeeec89c40b742dcb24abe9daf4324c7af875926
Adrian <adrianahmadi06@gmail.com>
2026-08-08 22:54:53 -0400

    add connection string

--- VideoGameCharacterApi/obj/Debug/net10.0/VideoGameCharacterApi.AssemblyInfo.cs
+++ VideoGameCharacterApi/obj/Debug/net10.0/VideoGameCharacterApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c449340ecd08967708037a6e9e8f289ae78426d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b18c9cddaf9ee36bd4222bf687796da34d7765f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
